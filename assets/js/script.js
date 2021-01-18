f88683859036c2fd7713a866394a84ae2a19e678
Jamie Zhang <jz@Jamies-MacBook-Air.local>
2021-01-17 23:54:12 -0500

    add files

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById("anim"),
+    rederer: "svg",
+    loop: false,
+    autoplay: true,
+    path: "assets/data.json",
+})
